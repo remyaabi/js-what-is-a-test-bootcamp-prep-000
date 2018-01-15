3a6da90fe62d072f7fb86809ff6c2b326de0e114
Remya Jacob <remyaabi@gmail.com>
2018-01-15 19:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+describe("Name",()=>{
+  it('returns "susan"',()=>{
+  expect(index.name).toEqual('Susan')
+  
+} )
+})
