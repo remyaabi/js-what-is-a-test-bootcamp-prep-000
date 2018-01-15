5db2364593e8deef533c74d7d8cc6b14e03a69fd
Remya Jacob <remyaabi@gmail.com>
2018-01-15 19:43:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,21 +5,3 @@ var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
 
-describe("Name",()=>{
-  it('returns "susan"',()=>{
-  expect(index.name).toEqual('Susan')
-  
-} )
-})
-
-describe("Height", () => {
-    it("is less than 40", () => {
-      expect(index.height).toBeLessThan(40)
-    })
-  })
-  describe("Message", () => {
-    it("gives the name and height", () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
-    })
-  })
